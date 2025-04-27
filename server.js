a9475bb1771fa3ca3c0e453a658a9222452ef347
mobina-soltani <mobinasoltanidev@gmail.com>
2025-04-26 21:46:47 -0700

    Implement login page

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`server is running on http:\\localhost:${PORT}`);
+	console.log(`server is running on http:\\localhost:${PORT}`);
 });
 
 app.get("/", (req, res) => {
-    res.redirect('/home');
-})
+	res.redirect("/home");
+});
 
-app.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+app.get("/home", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/login", (req, res) => {
+	res.sendFile(__dirname + "/login.html");
+});
+
+const userArr = [
+	{ username: "admin1", password: "admin1" },
+	{ username: "admin2", password: "admin12" },
+	{ username: "user1", password: "user1" },
+	{ username: "user2", password: "user2" },
+	{ username: "user3", password: "user3" },
+];
+
+app.use(express.urlencoded({ extended: true }));
+app.post("/login", (req, res) => {
+	const { username, password } = req.body;
+	const user = userArr.find(
+		(user) => user.username === username && user.password === password
+	);
+
+	if (user) {
+		res.send(`welcome ${username}`);
+	} else {
+		res.status(401).send("invalid credentials");
+	}
+});
